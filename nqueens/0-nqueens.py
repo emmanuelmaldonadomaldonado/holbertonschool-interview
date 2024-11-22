60468336058e40a8762431cbd19d4e24f160715b
Emmanuel Maldonado <maldonadomaldonadoemmanuel227@gmail.com>
2024-11-22 10:47:30 -0400

    .

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,145 +1,72 @@
 #!/usr/bin/python3
 """
-0. N queens
+Script that solves the N queens problem based on the general Backtracking
+algorithm.
+
+This is:
+procedure bt(c) is
+    if reject(P, c) then return
+    if accept(P, c) then output(P, c)
+    s  first(P, c)
+    while s  NULL do
+        bt(s)
+        s  next(P, s)
 """
 import sys
 
 
-def backtracking(board, row=0, column=0):
+def valid_pos(solution, pos):
     """
-    method that uses the backtracking algorithm to obtain
-    all possible solutions
+    Function that verifies if the position is valid
     """
-    # si llegamos a la ultima fila se imprime el resultado
-    if row == len(board):
-        print_result(board)
-        # luego continua buscando mas soluciones
-        return
-
-    if row == 0:
-        # si es la primera fila, no hay necesidad de comprobar
-        for column in range(len(board)):
-            board[row][column] = 1
-            backtracking(board, row + 1, 0)
-            board[row][column] = 0
-
-    else:
-        for column in range(len(board)):
-            # a partir de la segunda fila se comprueba si se puede colocar
-            if comprobation(board, row, column):
-                board[row][column] = 1
-                backtracking(board, row + 1, 0)
-                board[row][column] = 0
-
-
-def comprobation(board, row, column):
-    """
-    method that checks if the queen is being attacked,
-    vertically or diagonally on both sides
-    """
-    # comprobacion de arriba hacia abajo verticalmente
-    for row_2 in range(len(board)):
-        if board[row_2][column] == 1:
+    for queen in solution:
+        if queen[1] == pos[1]:
             return False
-
-    row_2 = row
-    column_2 = column
-    # comprobacion por si la posicion es el extremo izquierda
-    if row != len(board) - 1 or column != 0:
-        # ubicando el extremo inicial de la diagonal mediante su ubicacion
-        while row_2 > 0 and column_2 > 0:
-            row_2 -= 1
-            column_2 -= 1
-
-        # comprobacion de diagonal de izquierda a derecha
-        while row_2 < len(board) and column_2 < len(board):
-            if board[row_2][column_2] == 1:
-                return False
-            row_2 += 1
-            column_2 += 1
-
-    row_2 = row
-    column_2 = column
-    # comprobacion por si la posicion es el extremo derecha
-    if row != len(board) - 1 or column != len(board) - 1:
-        # ubicando el extremo inicial de la diagonal mediante su ubicacion
-        while row_2 > 0 and column_2 < len(board) - 1:
-            row_2 -= 1
-            column_2 += 1
-
-        # comprobacion de diagonal de derecha a izquierda
-        while row_2 < len(board) and column_2 >= 0:
-            if board[row_2][column_2] == 1:
-                return False
-            row_2 += 1
-            column_2 -= 1
-
-    # si pasa todas las pruebas entonces retornamos True
+        if (queen[0] + queen[1]) == (pos[0] + pos[1]):
+            return False
+        if (queen[0] - queen[1]) == (pos[0] - pos[1]):
+            return False
     return True
 
 
-def print_result(board):
+def solve_queens(row, n, solution):
     """
-    Method that prints the location of the queen
-    through its row and column
+    Function that finds the solution recursively, from the root down
     """
-    result = []
-    for row in board:
-        result.append([board.index(row), row.index(1)])
+    if (row == n):
+        print(solution)
+    else:
+        for col in range(n):
+            pos = [row, col]
+            if valid_pos(solution, pos):
+                solution.append(pos)
+                solve_queens(row + 1, n, solution)
+                solution.remove(pos)
 
-    print(result)
 
-
-def validate_integer(number):
+def main(n):
     """
-
+    Main function
     """
+    solution = []
+    """ From root(0) down(n) """
+    solve_queens(0, n, solution)
+
+
+if __name__ == '__main__':
+    """ Validate the arguments from OS """
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
     try:
-        # Intentamos convertir el argumento a entero
-        number = int(number)
-        # Si la conversión fue exitosa, retornamos el número
-        return number
-    except ValueError:
-        # Si la conversión falló, significa que el argumento no es un entero
-        # Entonces retornamos 1
-        return 1
-    except TypeError:
-        # Si ocurre un TypeError, significa que el argumento es None
-        # Entonces también retornamos 1
-        return 1
+        i = int(sys.argv[1])
+    except BaseException:
+        print('N must be a number')
+        sys.exit(1)
+    i = int(sys.argv[1])
+    if i < 4:
+        print('N must be at least 4')
+        sys.exit(1)
 
-
-# Obtenemos el número de argumentos y los argumentos
-argc = len(sys.argv)
-argv = sys.argv
-
-# Verificamos si se han proporcionado dos argumentos
-if argc != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-# Verificamos si el argumento N es un entero
-n = validate_integer(argv[1])
-
-if n == 1:
-    print("N must be a number")
-    sys.exit(1)
-
-# Verificamos si N es mayor o igual a 4
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-# Si llegamos aquí, significa que los argumentos son válidos
-# Podemos continuar con el programa...
-
-board = []
-row = []
-
-for i in range(n):
-    for j in range(n):
-        row.append(0)
-    board.append(row)
-    row = []
-
-backtracking(board)
+    """ Calling the main function """
+    main(i)
