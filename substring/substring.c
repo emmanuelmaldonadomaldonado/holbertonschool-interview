0bfa599cd78250d7595b4645a25cd30bae0e10b2
Emmanuel Maldonado <maldonadomaldonadoemmanuel227@gmail.com>
2025-04-18 20:43:58 -0400

    refactor substring functions: simplify find_substring logic and remove unused prototypes

--- substring/substring.c
+++ substring/substring.c
@@ -1,128 +1,102 @@
+#include <stdlib.h>
+#include <string.h>
 #include "substring.h"
 
 /**
- * find_substring - finds all possible substrings containing list of words
- * @s: the string to scan
- * @words: the array of words all substrings must be concatenation of
- * @nb_words: the number of elements in the array words
- * @n: pointer to which to store the number of elements in the returned array
- *
- * Return: array storing each index in s at which a substring was found
+ * struct WordCount - Simple structure to count words
+ * @word: the word
+ * @count: how many times it should appear
+ */
+typedef struct WordCount
+{
+    char *word;
+    int count;
+} WordCount;
+
+/**
+ * find_word - finds a word in the WordCount array
+ */
+int find_word(WordCount *wc, int size, const char *word)
+{
+    for (int i = 0; i < size; i++)
+        if (strcmp(wc[i].word, word) == 0)
+            return i;
+    return -1;
+}
+
+/**
+ * find_substring - main logic to find valid starting indexes
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int word = 0, i = 0, match = 0, count = 0, j = 0, k = 0, I = 0;
-    int *word_used = NULL, *indices = NULL;
+    int word_len, total_len, s_len, *indices = NULL, idx_count = 0;
+    WordCount *expected, *seen;
+    char *word;
 
     *n = 0;
-    if (words == NULL)
-        return (NULL);
-    j = (nb_words * string_length(words[0]));
-    i = allocate(&word_used, nb_words, &indices, (string_length(s) / j) + 1);
-    if (i == -1)
-        return (NULL);
-    for (i = 0; s[i]; i++)
+    if (!s || !words || nb_words == 0)
+        return NULL;
+
+    word_len = strlen(words[0]);
+    total_len = word_len * nb_words;
+    s_len = strlen(s);
+
+    expected = malloc(sizeof(WordCount) * nb_words);
+    if (!expected)
+        return NULL;
+
+    // Count expected words
+    int unique = 0;
+    for (int i = 0; i < nb_words; i++)
     {
-        I = i;
-        for (reset(&word_used, nb_words), word = 0; word < nb_words && s[i]; word++)
+        int j = find_word(expected, unique, words[i]);
+        if (j != -1)
+            expected[j].count++;
+        else
         {
-            match = string_compare(s, i, words[word]);
-            while (match != 0)
-            {
-                word_used[word] = 1;
-                i += match;
-                if (s[i] == '\0')
-                    break;
-                for (count = 0, word = 0; word < nb_words; word++)
-                {
-                    if (word_used[word] != -1)
-                        count++;
-                    match = string_compare(s, i, words[word]);
-                    if (match != 0)
-                        break;
-                }
-                if (count == nb_words)
-                    indices[k++] = (i - j);
-            }
+            expected[unique].word = strdup(words[i]);
+            expected[unique].count = 1;
+            unique++;
         }
-        if (i != I || s[i] == '\0')
-            i--;
     }
-    free(word_used);
-    *n = k;
-    return (indices);
-}
 
-/**
- * allocate - allocates memory for int arrays
- * @word_used: pointer to first memory to allocate
- * @nb_words: number of words to allocate space for
- * @indices: pointer to second memory to allocate
- * @max_index: maximum number of indices possible to allocate for
- *
- * Return: -1 if failed, 0 if successful
- */
-int allocate(int **word_used, int nb_words, int **indices, int max_index)
-{
-    *word_used = malloc(sizeof(int) * nb_words);
-    if (*word_used == NULL)
-        return (-1);
-    reset(word_used, nb_words);
-    *indices = malloc(sizeof(int) * max_index);
-    if (*indices == NULL)
+    // Sliding window
+    for (int i = 0; i <= s_len - total_len; i++)
     {
-        free(word_used);
-        return (-1);
+        seen = malloc(sizeof(WordCount) * unique);
+        for (int k = 0; k < unique; k++)
+        {
+            seen[k].word = expected[k].word;
+            seen[k].count = 0;
+        }
+
+        int j;
+        for (j = 0; j < nb_words; j++)
+        {
+            word = strndup(s + i + j * word_len, word_len);
+            int pos = find_word(expected, unique, word);
+            free(word);
+            if (pos == -1)
+                break;
+
+            seen[pos].count++;
+            if (seen[pos].count > expected[pos].count)
+                break;
+        }
+
+        if (j == nb_words)
+        {
+            indices = realloc(indices, sizeof(int) * (idx_count + 1));
+            indices[idx_count++] = i;
+        }
+
+        free(seen);
     }
-    reset(indices, max_index);
-    return (0);
-}
 
-/**
- * string_compare - compares two strings to see if one contains the other
- * @s1: the first string to compare, the longer of the strings
- * @i: the index of the first string to start from
- * @s2: the second string to compare, the shorter of the strings
- *
- * Return: length of the shorter word contained or 0 if no match
- */
-int string_compare(char const *s1, unsigned int i, char const *s2)
-{
-    unsigned int j = 0;
+    for (int i = 0; i < unique; i++)
+        free(expected[i].word);
+    free(expected);
 
-    for (j = 0; s1[i] && s2[j]; i++, j++)
-    {
-        if (s1[i] != s2[j])
-            return (0);
-    }
-    return (j);
-}
-
-/**
- * string_length - finds the length of a string
- * @str: the string to get length of
- *
- * Return: length of the string
- */
-int string_length(char const *str)
-{
-    unsigned int i = 0;
-
-    for (i = 0; str[i]; i++)
-        continue;
-    return (i);
-}
-
-/**
- * reset - sets all elements of array to -1
- * @array: pointer to the array to reset
- * @size: the size of the array to reset
- *
- */
-void reset(int **array, int size)
-{
-    int i = 0;
-
-    for (i = 0; i < size; i++)
-        (*array)[i] = -1;
+    *n = idx_count;
+    return indices;
 }
